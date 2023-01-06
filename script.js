dcac245bd563115427f84cd58a57af59e8f45d82
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-01-05 17:13:27 -0700

    Create basic html and javascript outline

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-let myLibrary = [];
+let library = [];
+const defaultData = [
+  {
+    title: "Eloquent JavaScript: A Modern Introduction to Programming",
+    author: "Marijn Haverbeke",
+    skill: "JavaScript",
+    status: "unread",
+  },
+  {
+    title: "The Principles of Object-Oriented JavaScript",
+    author: "Nicholas C. Zakas",
+    skill: "JavaScript",
+    status: "read",
+  },
+  {
+    title: "You Don't Know JS",
+    author: "Kyle Simpson",
+    skill: "JavaScript",
+    status: "read",
+  },
+];
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const skill = document.querySelector("#skill");
+const status = document.querySelector("#status");
 
-function Book() {
-  // the constructor...
+function Book(title, author, skill, status) {
+  this.title = title;
+  this.author = author;
+  this.skill = skill;
+  this.status = status;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  const newBook = new Book(
+    title.value,
+    author.value,
+    skill.value,
+    status.value
+  );
+  library.push(newBook);
+  updateLibrary();
+}
+
+function updateLibrary() {
+  // loop through array and add books to page
 }
